958ae3c109bb4679c9b70edea88a8b85e9bf618b
Simon Petrac <pfoerdie@gmail.com>
2021-07-26 10:40:45 +0200

    added constants: OS platform and architecture

--- src/core.util.constants.js
+++ src/core.util.constants.js
@@ -1,7 +1,12 @@
+const os = require('os');
+
 exports.FUA_REMOTES   = process.env.FUA_REMOTES;
 exports.FUA_RESOURCES = process.env.FUA_RESOURCES;
 exports.FUA_JS_LIB    = process.env.FUA_JS_LIB;
 exports.FUA_JS_APP    = process.env.FUA_JS_APP;
+
+exports.OS_PLATFORM     = os.platform();
+exports.OS_ARCHITECTURE = os.arch();
 
 exports.$$iterator      = Symbol.iterator;
 exports.$$asyncIterator = Symbol.asyncIterator;
