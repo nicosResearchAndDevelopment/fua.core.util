d939b6ea0b4fc395a9800d42d2456b9d2fbc4549
Simon Petrac <pfoerdie@gmail.com>
2021-12-01 12:15:15 +0100

    wip

--- lab/TicketQueue.js
+++ lab/TicketQueue.js
@@ -1,51 +1,66 @@
+const
+    EventEmitter = require('events'),
+    events       = {
+        activate: 'activate',
+        close:    'close'
+    };
+
 class Ticket {
 
-    // TODO
+    #ticketQueue  = null;
+    #readyPromise = null;
+
+    constructor(ticketQueue) {
+        this.#ticketQueue  = ticketQueue;
+        this.#readyPromise = new Promise((resolve, reject) => {
+            const activate = (ticket) => {
+                if (ticket !== this) return;
+                this.#ticketQueue.off(events.activate, activate);
+                this.#readyPromise = Promise.resolve(this);
+                resolve(this);
+            };
+            this.#ticketQueue.on(events.activate, activate);
+        });
+    } // Ticket#constructor
+
+    get ready() {
+        return this.#readyPromise;
+    } // Ticket#ready
+
+    close() {
+        this.#ticketQueue.emit(events.close, this);
+    } // Ticket#close
 
 } // Ticket
 
-class TicketRequest {
+class TicketQueue extends EventEmitter {
 
-    #promise;
-    #resolve;
-    #reject;
-    #timeout;
+    #activeTicket  = null;
+    #queuedTickets = [];
 
     constructor() {
-        this.#promise = new Promise((resolve, reject) => {
-            this.#resolve = resolve;
-            this.#reject  = reject;
+        super();
+        this.on(events.close, (ticket) => {
+            if (ticket === this.#activeTicket) {
+                this.#activeTicket = this.#queuedTickets.shift() || null;
+                if (this.#activeTicket) this.emit(events.activate, this.#activeTicket);
+            } else {
+                const index = this.#queuedTickets.indexOf(ticket);
+                if (index >= 0) this.#queuedTickets.splice(index, 1);
+            }
         });
-    } // TicketRequest#constructor
+    } // TicketQueue#constructor
 
-    resolve() {
-        if (this.#timeout) clearTimeout(this.#timeout);
-        this.#resolve(new Ticket());
-    } // TicketRequest#resolve
-
-    reject(err) {
-        if (this.#timeout) clearTimeout(this.#timeout);
-        this.#reject(err);
-    } // TicketRequest#reject
-
-    setTimeout() {
-        // TODO
-    } // TicketRequest#reject
-
-    // TODO
-
-} // TicketRequest
-
-class TicketQueue {
-
-    #activeTicket = null;
-    #ticketQueue  = [];
-
-    async requestTicket() {
-        // TODO
+    requestTicket() {
+        const ticket = new Ticket(this);
+        if (this.#activeTicket) {
+            this.#queuedTickets.push(ticket);
+        } else {
+            this.#activeTicket = ticket;
+            this.emit(events.activate, ticket);
+        }
+        return ticket;
     } // TicketQueue#requestTicket
-
-    // TODO
 
 } // TicketQueue
 
