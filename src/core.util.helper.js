190e0c8e72d3ebb8b3c2a607d54649316b5ca948
Simon Petrac <pfoerdie@gmail.com>
2023-11-08 14:28:43 +0100

    added callbacky and callbackify

--- src/core.util.helper.js
+++ src/core.util.helper.js
@@ -28,6 +28,36 @@ exports.promisify = function (fn) {
 };
 
 /**
+ * @param {Function} fn
+ * @param {...any} args
+ * @param {Function} cb
+ * @returns {void}
+ */
+exports.callbacky = function (fn, ...args /*, cb*/) {
+    const cb = args.pop();
+    try {
+        const result = fn.apply(this, args);
+        if (result instanceof Promise) result
+            .then(res => cb(null, res))
+            .catch(err => cb(err));
+        else cb(null, result);
+    } catch (err) {
+        cb(err);
+    }
+};
+
+/**
+ * @param {Function} fn
+ * @returns {function(...args: any, cb: Function): void}
+ */
+exports.callbackify = function (fn) {
+    return function (...args) {
+        args.unshift(fn);
+        return exports.callbacky.apply(this, args);
+    };
+};
+
+/**
  * Returns a string representing the type of the given value.
  * @param {any} value
  * @returns {string}
