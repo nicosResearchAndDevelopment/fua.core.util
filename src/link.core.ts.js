e0c3edb980b43148d54ae595011859be069b48e0
Simon Petrac <pfoerdie@gmail.com>
2023-11-29 17:19:07 +0100

    fixed seconds conversion for duration

--- src/link.core.ts.js
+++ src/link.core.ts.js
@@ -2,6 +2,7 @@ const is = require('@nrd/fua.core.is');
 const ts = require('@nrd/fua.core.ts');
 
 const _secondsToMS    = (value) => is.number(value) ? 1e3 * value : value;
+const _msToSeconds    = (value) => is.number(value) ? value / 1e3 : value;
 exports.unixTime      = (rounded = false) => rounded ? ts.unix.rounded() : ts.unix();
 exports.time          = (value) => ts.time(_secondsToMS(value));
 exports.localTime     = (value) => ts.time.local(_secondsToMS(value));
@@ -12,5 +13,5 @@ exports.utcDate       = (value) => ts.date.utc(_secondsToMS(value));
 exports.dateTime      = (value) => ts.dateTime(_secondsToMS(value));
 exports.localDateTime = (value) => ts.dateTime.local(_secondsToMS(value));
 exports.utcDateTime   = (value) => ts.dateTime.utc(_secondsToMS(value));
-exports.duration      = (value, reference) => ts.duration(_secondsToMS(value), _secondsToMS(reference));
+exports.duration      = (value, reference) => _msToSeconds(ts.duration(_secondsToMS(value), _secondsToMS(reference)));
 exports.pause         = (value) => ts.pause(_secondsToMS(value));
